094a4e4eb15f8e4e6437a7ad6b261f0959a9cca0
cls-cheung <sabrinacheung1029@gmail.com>
2024-01-15 14:27:11 +0000

    added generic error message for task 2

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { getTopics } = require("./controller");
-
+const { getTopics, invalidPath } = require("./controller");
 const app = express();
+
+
 
 app.get("/api/topics", getTopics)
 
+app.all('*',invalidPath)
+
+
+  
 module.exports = app;
--- controller.js
+++ controller.js
@@ -3,10 +3,15 @@ const { fetchTopics } = require("./model");
 exports.getTopics = async (req, res) => {
   try {
     const topic = await fetchTopics();
-    res.status("200").send({topic});
-  } catch (err){
+    res.status("200").send({ topic });
+  } catch (err) {
     (err) => {
       console.log(err);
+      return next(err);
     };
   }
+};
+
+exports.invalidPath = (req, res) => {
+  res.status(404).send({ msg: "invalid endpoint" });
 };
